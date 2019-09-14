340065246dbbe7332d660805f7256521f1138dab
catyeo18 <catherineyeo2018@gmail.com>
2019-09-14 15:37:32 -0400

    Happy/Sad/Angry analysis

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from itertools import islice
 
 # Import data, field set as Emoji 
 df = pd.read_csv('Emoji_Sentiment_Data_1.csv', index_col='Emoji')
@@ -9,8 +10,8 @@ df = pd.read_csv('Emoji_Sentiment_Data_1.csv', index_col='Emoji')
 ########## FUNCTIONS ############
 #################################
 
-# Input: emoji itself
-# Output: positive/negative/neutral
+# Input: emoji 
+# Output: positive/negative/neutral sentiment
 def get_sentiment(emoji):
 	# find max of the pos/neg/neutral percentages
 	row_data = df.loc[emoji]
@@ -26,9 +27,33 @@ def get_sentiment(emoji):
 	else:
 		return "Negative"
 
+# Input: emoji
+# Output: full name
 def get_emoji_name(emoji):
 	row_data = df.loc[emoji]
 	return row_data['Emoji_Name']
+
+# More advanced than get_sentiment
+# by differentiating emoticons (faces) from other emoji icons
+HAPPY = ('HAPPY', 'JOY', 'CHEER', 'SMILE', 'SMILING', 'GRIN', 'GRINNING', 'WINKING', 'SMIRKING')
+SAD = ('SAD', 'TIRED', 'WEARY', 'CRY', 'CRYING', 'PERSEVERING')
+ANGRY = ('ANGRY', 'MAD', 'UPSET', 'UNAMUSED')
+
+def get_emotion(emoji):
+	row_data = df.loc[emoji]
+
+	# if it is an emoticon, further analysis
+	if row_data['Emoji_Type'] == 'Emoticons':
+		name = get_emoji_name(emoji).split()
+		for word in name:
+			if word in HAPPY:
+				return "Happy"
+			if word in SAD:
+				return "Sad"
+			if word in ANGRY:
+				return "Angry"
+
+	return get_sentiment(emoji)
 	
 #################################
 ########## TESTING ##############
@@ -46,4 +71,10 @@ def test_get_emoji_name():
 		print(emoji)
 		print (get_emoji_name(emoji))
 	
-test_get_emoji_name()
+def test_get_emotion():
+	for emoji, data in islice(df.iterrows(), 25):
+		print(emoji)
+		print (get_emotion(emoji))
+		print()
+
+test_get_emotion()
