b132b21515e74219abc6ef32976ee4e1d5e1ae76
catyeo18 <catherineyeo2018@gmail.com>
2019-09-14 20:58:16 -0400

    Update scrape_youtube_music.py

--- scrape_youtube_music.py
+++ scrape_youtube_music.py
@@ -3,14 +3,26 @@ from bs4 import BeautifulSoup
 import csv 
 import random
   
-def return_playlist():
-	return 
-
-
 
 # Input: Youtube Playlist URL
 def return_track(emotion):
-	URL = "https://youtube.com/playlist?list=RDCLAK5uy_kJWGcrtTC_zrbD6rKkBvOcht_vzijhX1A"
+	# URL = "https://youtube.com/playlist?list=RDCLAK5uy_kJWGcrtTC_zrbD6rKkBvOcht_vzijhX1A"
+	
+	# happyURL = "https://youtube.com/playlist?list=RDCLAK5uy_kJWGcrtTC_zrbD6rKkBvOcht_vzijhX1A"
+	# sadURL = "https://youtube.com/playlist?list=RDCLAK5uy_kIlNbVLt0zdMDvQJgf9Ilzbpy9KSk1xE4"
+	# angryURL
+	# neutralURL = "https://www.youtube.com/playlist?list=RDCLAK5uy_kb7EBi6y3GrtJri4_ZH56Ms786DFEimbM"
+	
+	if emotion == 'HAPPY' or emotion == 'POSITIVE':
+		URL = "https://www.youtube.com/playlist?list=PLhGO2bt0EkwvRUioaJMLxrMNhU44lRWg8"
+	elif emotion == 'SAD' or emotion == 'NEGATIVE':
+		URL = "https://youtube.com/playlist?list=RDCLAK5uy_kIlNbVLt0zdMDvQJgf9Ilzbpy9KSk1xE4"
+	elif emotion == 'ANGRY':
+		URL = 'https://www.youtube.com/playlist?list=RDCLAK5uy_mPqT1su7A0jeg2kTWdhj7I6JyDfwD4O-4'
+	else:
+		URL = "https://www.youtube.com/playlist?list=RDCLAK5uy_kb7EBi6y3GrtJri4_ZH56Ms786DFEimbM"
+
+
 	r = requests.get(URL) 
 	soup = BeautifulSoup(r.content, 'html.parser') 
 	# print(soup)
